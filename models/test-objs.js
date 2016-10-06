4a7ea338478693460ac893c0005e99421d7798d4
Andrzej Krzastek <krzasteka@gmail.com>
2016-10-05 18:23:38 -0700

    added test-objs

---
+++ models/test-objs.js
@@ -0,0 +1,51 @@
+company
+{
+	"name": "Andrew",
+	"email":"krzasteka@gmail.com",
+	"address":{
+		"street":"6208 Payton Ave",
+		"unit":"1",
+		"city":"Irvine", 
+		"state":"California",
+		"zipcode":"92620"
+	},
+	"phone":"2014008888",
+	"fax":"2010045523",
+	"contact"[
+		{
+			"Rafal":"9143337768"
+		}
+	]
+}
+
+work order
+{
+		"job_name": "MC-INS",
+		"address": {
+			"street":"6th Avenue",
+			"unit":"Ste. 7",
+			"city":"Garfield",
+			"state":"NJ",
+			"zipcode":"92622"
+		},
+		"phone":"201-558-9622",
+		"client":"Manhatan Crew",
+		"contact":[
+			{
+				"name":"Tom Wozek",
+				"phone":"201-523-6889"			
+			}
+		],
+		"data":[	
+				{
+					"name":"ph",
+					"value":"6.0"
+				},
+				{
+					"name":"oH",
+					"value":"1.0"
+				}
+			],
+		"notes":["something","something1"],
+		"remarks":"this was an easy job"
+	}
